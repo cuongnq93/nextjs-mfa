4ed0d3a8654bfa561d68ebc2a48172c3aaf201ab
wl-nguyen-cuong <nguyen-cuong@web-life.co.jp>
2024-09-26 14:14:47 +0700

    feat: remove unused api

--- src/app/api/auth/verify/route.ts
+++
@@ -1,9 +0,0 @@
-import { verifyTwoFactorAuthenticationCode } from '@/app/services/authentication';
-
-export const revalidate = 0;
-
-export async function POST({ body }: { body: { base32: string; code: string } }) {
-  return Response.json({
-    verified: verifyTwoFactorAuthenticationCode(body.base32, body.code),
-  });
-}
